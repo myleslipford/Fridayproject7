536f8a0f2a390942748ef304251c5b78b5c1b87b
myleslipford <myles.lipford12@gmail.com>
2024-03-31 16:45:26 -0500

    Created password label and entry

--- SignInPage.py
+++ SignInPage.py
@@ -10,3 +10,8 @@ email_label = tk.Label(root, text="Email:")
 email_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.E)
 email_entry = tk.Entry(root)
 email_entry.grid(row=0, column=1, padx=5, pady=5)
+
+password_label = tk.Label(root, text="Password:")
+password_label.grid(row=1, column=0, padx=5, pady=5, sticky=tk.E)
+password_entry = tk.Entry(root, show="*")
+password_entry.grid(row=1, column=1, padx=5, pady=5)
