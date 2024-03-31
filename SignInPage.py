fb4e330cdfe025a196c340857fd44eebac769c39
myleslipford <myles.lipford12@gmail.com>
2024-03-31 16:45:07 -0500

    Created email label and entry

--- SignInPage.py
+++ SignInPage.py
@@ -5,3 +5,8 @@ import sqlite3
 
 root = tk.Tk()
 root.title("Sign In")
+
+email_label = tk.Label(root, text="Email:")
+email_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.E)
+email_entry = tk.Entry(root)
+email_entry.grid(row=0, column=1, padx=5, pady=5)
